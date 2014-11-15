cacced0218ec31e60933dccdeee0ec66d3e403d0
way2muchnoise <jens.baetens@live.be>
2014-11-15 15:44:04 +0100

    adds read functions to the message helper
    fixes some derps

--- src/main/java/minechemV6Base/utils/MessageHelper.java
+++ src/main/java/minechemV6Base/utils/MessageHelper.java
@@ -21,14 +21,43 @@ public class MessageHelper
         }
     }
 
+    public static ItemStack writeItemStack(ByteBuf buf)
+    {
+        Item item = Item.getItemById(buf.readInt());
+        int stackSize = buf.readInt();
+        int damage = buf.readInt();
+
+        ItemStack itemStack = new ItemStack(item, stackSize, damage);
+
+        if (buf.readBoolean())
+        {
+            itemStack.stackTagCompound = readNBTTagCompound(buf);
+        }
+
+        return itemStack;
+    }
+
     public static void writeNBTTagCompound(ByteBuf buf, NBTTagCompound tagCompound)
     {
         buf.writeInt(tagCompound.func_150296_c().size()); // count of tags
         for (Object o : tagCompound.func_150296_c())
         {
             String tagName = String.valueOf(o);
+            writeString(buf, tagName);
             writeNBTTag(buf, tagCompound.getTag(tagName));
         }
+    }
+
+    public static NBTTagCompound readNBTTagCompound(ByteBuf buf)
+    {
+        int count = buf.readInt(); // count of tags
+        NBTTagCompound tagCompound = new NBTTagCompound();
+        for (int i = 0; i < count; i++)
+        {
+            String tagName = readString(buf);
+            tagCompound.setTag(tagName, readNBTTag(buf));
+        }
+        return tagCompound;
     }
 
     public static void writeNBTTag(ByteBuf buf, NBTBase tag)
@@ -55,6 +84,7 @@ public class MessageHelper
                 buf.writeDouble(((NBTTagDouble) tag).func_150286_g());
                 break;
             case 7:
+                buf.writeInt(((NBTTagByteArray) tag).func_150292_c().length);
                 buf.writeBytes(((NBTTagByteArray) tag).func_150292_c());
                 break;
             case 8:
@@ -65,17 +95,51 @@ public class MessageHelper
             case 10:
                 writeNBTTagCompound(buf, (NBTTagCompound)tag);
             case 11:
-                writeIntArray(buf, ((NBTTagIntArray)tag).func_150302_c());
+                writeIntArray(buf, ((NBTTagIntArray) tag).func_150302_c());
                 break;
             default:
                 break;
         }
     }
 
+    public static NBTBase readNBTTag(ByteBuf buf)
+    {
+        switch (buf.readByte())
+        {
+            case 1:
+                return new NBTTagByte(buf.readByte());
+            case 2:
+                return new NBTTagShort(buf.readShort());
+            case 3:
+                return new NBTTagInt(buf.readInt());
+            case 4:
+                return new NBTTagLong(buf.readLong());
+            case 5:
+                return new NBTTagFloat(buf.readFloat());
+            case 6:
+                return new NBTTagDouble(buf.readFloat());
+            case 7:
+                int length = buf.readInt();
+                return new NBTTagByteArray(buf.readBytes(length).array());
+            case 8:
+                return new NBTTagString(readString(buf));
+            case 9:
+                return readNBTTagList(buf);
+            case 10:
+                return readNBTTagCompound(buf);
+            case 11:
+                return new NBTTagIntArray(readIntArray(buf));
+            default:
+                return null;
+        }
+    }
+
     public static void writeNBTTagList(ByteBuf buf, NBTTagList tagList)
     {
+        buf.writeInt(tagList.tagCount());
         for (int i = 0; i < tagList.tagCount(); i++)
         {
+            buf.writeInt(tagList.func_150303_d());
             switch (tagList.func_150303_d())
             {
                 case 5:
@@ -89,12 +153,45 @@ public class MessageHelper
                     break;
                 case 10:
                     writeNBTTagCompound(buf, tagList.getCompoundTagAt(i));
+                    break;
                 case 11:
                     writeIntArray(buf, tagList.func_150306_c(i));
+                    break;
                 default:
                     break;
             }
         }
+    }
+
+    public static NBTTagList readNBTTagList(ByteBuf buf)
+    {
+        int count = buf.readInt();
+        NBTTagList list = new NBTTagList();
+        for (int i = 0; i < count; i++)
+        {
+            int type = buf.readInt();
+            switch (type)
+            {
+                case 5:
+                    list.appendTag(new NBTTagFloat(buf.readFloat()));
+                    break;
+                case 6:
+                    list.appendTag(new NBTTagDouble(buf.readDouble()));
+                    break;
+                case 8:
+                    list.appendTag(new NBTTagString(readString(buf)));
+                    break;
+                case 10:
+                    list.appendTag(readNBTTagCompound(buf));
+                    break;
+                case 11:
+                    list.appendTag(new NBTTagIntArray(readIntArray(buf)));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return list;
     }
 
     public static void writeString(ByteBuf buf, String string)
@@ -103,10 +200,25 @@ public class MessageHelper
         buf.writeBytes(string.getBytes());
     }
 
+    public static String readString(ByteBuf buf)
+    {
+        int length = buf.readInt();
+        return new String(buf.readBytes(length).array());
+    }
+
     public static void writeIntArray(ByteBuf buf, int[] array)
     {
         buf.writeInt(array.length);
         for (int i : array)
             buf.writeInt(i);
+    }
+
+    public static int[] readIntArray(ByteBuf buf)
+    {
+        int length = buf.readInt();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++)
+            array[i] = buf.readInt();
+        return array;
     }
 }

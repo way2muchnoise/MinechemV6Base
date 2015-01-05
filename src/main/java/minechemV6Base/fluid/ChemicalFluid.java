40b2d22c57d2e77d82cb87f535954ac28bfafef7
hilburn <charliepaterson89@gmail.com>
2015-01-05 21:38:38 +0000

    Stackable fluid container item
    Holds a set amount per item
    Uses NBT to store the fluid type and stacksize to store the amount
    Use as base class for Minechem test tubes/beakers?

---
+++ src/main/java/minechemV6Base/fluid/ChemicalFluid.java
@@ -0,0 +1,11 @@
+package minechemV6Base.fluid;
+
+import net.minecraftforge.fluids.Fluid;
+
+public class ChemicalFluid extends Fluid
+{
+    public ChemicalFluid(String fluidName)
+    {
+        super(fluidName);
+    }
+}

5a3e64891f2fcf146a00aaf7619ae8cf70044814
yushijinhun <yushijinhun@gmail.com>
2015-01-23 22:27:36 +0800

    do not ask me any more

--- src/main/java/minechemV6Base/chemical/ChemicalBase.java
+++ src/main/java/minechemV6Base/chemical/ChemicalBase.java
@@ -21,8 +21,8 @@ public abstract class ChemicalBase implements IChemical, IState, IRadioactive
         this.name = name;
         this.temp = 293;
         this.halfLife = -1L;
-        this.meltingPoint = 500;
-        this.boilingPoint = 1000; // don't ask me where the values are from
+        this.meltingPoint = 0;
+        this.boilingPoint = 1000; // don't ask me where the values are from at all
     }
 
     public ChemicalBase(String name, int temp)
